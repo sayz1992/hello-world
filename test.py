18b6bb94d3999f58f0e06a45fabaa0f8256aef70
Swagata Kar <sayz1992@gmail.com>
2018-07-20 18:16:07 +0530

    checking python code to print a pattern

---
+++ test.py
@@ -0,0 +1,22 @@
+i = 1
+j = 2
+
+while i <=5:
+    a = " "*j+"*"*i+" "*j
+    print a
+
+    i+= 2
+    j-= 1
+
+    #if i==5:
+        #break
+i = 3
+j = 1
+
+while i >=1:
+    a = " "*j+"*"*i+" "*j
+    print a
+
+    i-= 2
+    j+= 1
+
