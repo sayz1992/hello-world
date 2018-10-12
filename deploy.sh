71a6d79a3d54e9bdeae7d1cb88a24a3ce1ce11e4
root <root@ip-172-31-31-221.ap-south-1.compute.internal>
2018-10-12 09:39:14 +0000

    modified deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 ENVIRONMENT=$1
 if [ $ENVIRONMENT = "QA" ];then
-        sshpass -p "ec2-user" scp target/ec2-userkart.war ec2-user@172-31-1-55:/home/ec2-user/Distros/apache-tomcat-8.5.34/webapps
-        sshpass -p "ec2-user" ssh ec2-user@172-31-1-55 "JAVA_HOME=/home/ec2-user/Distros/jdk1.8.0_151" "/home/ec2-user/Distros/apache-tomcat-8.5.23/bin/startup.sh"
+        sshpass -p "Swagat@963" scp target/gamutkart.war sayz@172.17.0.2:/home/sayz/apache-tomcat-8.5.34/webapps
+        sshpass -p "Swagat@963" ssh sayz1992@172.17.0.2 "JAVA_HOME=/home/ec2-user/Distros/jdk1.8.0_151" "/home/ec2-user/Distros/apache-tomcat-8.5.23/bin/startup.sh"
 
 elif  [ $ENVIRONMENT = "SIT" ];then
-        sshpass -p "ec2-user" scp target/ec2-userkart.war ec2-user@172.31.31.221:/home/ec2-user/Distros/apache-tomcat-8.5.34/webapps
+        sshpass -p "ec2-user" scp target/gamutkart.war ec2-user@172.31.31.221:/home/ec2-user/Distros/apache-tomcat-8.5.34/webapps
         sshpass -p "ec2-user" ssh ec2-user@172.31.31.221 "JAVA_HOME=/home/ec2-user/Distros/jdk1.8.0_151" "/home/ec2-user/Distros/apache-tomcat-8.5.23/bin/startup.sh"
 fi
